9ad05522f8c5db48f9d1599372d98afa1bc6b0ab
Atif <atif.hyd123@gmail.com>
2025-09-01 22:47:32 +0530

    second

---
+++ Module 1/Primitive Datatypes/Common.cpp
@@ -0,0 +1,19 @@
+//Integer types
+int a = 10; // regular integer
+long long int b = 10000000000; // larger integer
+unsigned int c = 42; // non-negative integer
+
+//Character type
+char d = 'A'; // single character
+
+//Boolean Type
+bool e = true; // boolean (true/false)
+
+//floating-point types 
+float f = 3.14f; // single-precision floating-point 
+double g = 3.141592653589793; // double-precision floating-point
+long double h = 3.141592653589793238462643383279502884L; // extended precision floating-point
+
+//special type
+void* ptr = nullptr; // void pointer (points to no type)
+
